1dc32d3c62196d5e860a1bac4e4a4a044495ec2f
limonadxax123 <deniskrutikov39@gmail.com>
2026-02-18 18:49:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -78,9 +78,5 @@ def handle_message(message):
 
 
 if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(
-        url=f"https://tgbot-1-vunr.onrender.com/{TG_TOKEN}"
-    )
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
